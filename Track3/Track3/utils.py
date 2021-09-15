9005cfc60e59b10529d8486d548a3c11c1c75d75
Zhangxm520 <48277018+Zhangxm520@users.noreply.github.com>
2021-09-15 22:15:42 +0800

    Add files via upload

---
+++ Track3/Track3/utils.py
@@ -0,0 +1,28 @@
+import torch
+import os
+import random
+import numpy as np
+
+def np2tensor(arrays,device='gpu'):
+    tensor=torch.from_numpy(arrays).type(torch.float)
+    return tensor.cuda() if device=='gpu' else tensor
+
+
+def mylog(*t,path=None):
+    t=" ".join([str(now) for now in t])
+    print(t)
+    if os.path.isfile(path) == False:
+        f = open(path, 'w+')
+    else:
+        f = open(path, 'a')
+    f.write(t + '\n')
+    f.close()
+
+def set_seed(seed):
+    torch.set_default_tensor_type(torch.cuda.FloatTensor)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    random.seed(seed)
