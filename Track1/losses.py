bb9ac7ac6b1a2bf4d3df4211a8673ad8a0167756
Zhangxm520 <48277018+Zhangxm520@users.noreply.github.com>
2022-05-20 17:52:48 +0800

    Update losses.py

--- Track1/losses.py
+++ Track1/losses.py
@@ -9,10 +9,7 @@ def contrastive_loss(x1,x2,beta=0.08):
 
     cosine_mat=torch.cosine_similarity(torch.unsqueeze(x1x2,dim=1),
                                        torch.unsqueeze(x1x2,dim=0),dim=2)/beta
-    mask=torch.eye(x1.size(0))
-    mask=torch.cat([mask,mask],dim=0)
-    mask = 1.0-torch.cat([mask, mask], dim=1)
-
+    mask=1.0-torch.eye(2*x1.size(0))
     numerators = torch.exp(torch.cosine_similarity(x1x2,x2x1,dim=1)/beta)
     denominators=torch.sum(torch.exp(cosine_mat)*mask,dim=1)
     return -torch.mean(torch.log(numerators/denominators),dim=0)
